193fc70f319957c0b15fd190864d172350d63fed
mjkim912 <sinceree912@gmail.com>
2023-07-21 18:11:11 +0900

    Day7

---
+++ 7일차/test/Facial_Recognition_Part2.py
@@ -0,0 +1,29 @@
+# python.exe -m pip install --upgrade pip
+# pip install opencv-contrib-python 모듈 설치
+# 에러발생시 삭제후 재설치
+
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+data_path = 'faces/'
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
+
+Training_Data, Labels = [], []
+
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+
+Labels = np.asarray(Labels, dtype=np.int32)
+
+model = cv2.face.LBPHFaceRecognizer_create()
+
+model.train(np.asarray(Training_Data), np.asarray(Labels))
+
+print("Model Training Complete!!!!!")
+
+
